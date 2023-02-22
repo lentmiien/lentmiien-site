731d0202ca38b8a704d662de25429d3ba06d88ba
lentmiien <lentmiien@gmail.com>
2023-02-22 17:09:17 +0900

    add pug and gtag

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+const path = require('path');
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello Lennart!"));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.get("/", (req, res) => res.render("index"));
 
 app.listen(8080, () => console.log("Server running on port 8080"));
