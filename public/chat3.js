8076d62369ec8ddc8fec80f106118695a494f37d
Lennart Granstrom <lentmiien@gmail.com>
2023-11-19 00:40:57 +0900

    popup

--- public/chat3.js
+++ public/chat3.js
@@ -109,3 +109,32 @@ function ChangeBranch(_id, branch) {
   console.log(_id, branch, head, id_to_index_map[head]);
   Populate(id_to_index_map[head]);
 }
+
+// Popup
+document.addEventListener("DOMContentLoaded", function() {
+  // Get the button that opens the popup
+  var btn = document.getElementById("openPopupBtn");
+
+  // Get the popup div
+  var popup = document.getElementById("popup");
+
+  // Get the <span> element that closes the popup
+  var span = document.getElementById("closePopupBtn");
+
+  // When the user clicks the button, open the popup
+  btn.onclick = function() {
+    popup.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the popup
+  span.onclick = function() {
+    popup.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the popup, close it
+  window.onclick = function(event) {
+    if (event.target == popup) {
+      popup.style.display = "none";
+    }
+  }
+});
