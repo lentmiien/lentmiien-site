2221886201f625690ddff70b4978c01af3ea10ef
Lennart <lentmiien@gmail.com>
2024-01-01 11:29:49 +0900

    Refactor cookingpubliccontroller for user validation
    Introduce user validation logic using a temporary user list for
    accessing specific content, and render the cooking request index based
    on user validation. This ensures that only valid users can view their
    own requests. No associated issues.

--- controllers/cookingpubliccontroller.js
+++ controllers/cookingpubliccontroller.js
@@ -1,6 +1,24 @@
 // Require necessary database models
-// const { ArticleModel } = require('../database');
+const { CookingCalendarModel, CookingRequestModel, Chat3KnowledgeModel } = require('../database');
+
+// This is temporary solution, so OK for now (plan to prepare database)
+const user_list = {
+  "nzz8gXilQBZ8nxS78b0UjhpPtnXqUMFW": "Maiko",
+  "JSqngWDQyGoIVohdrG96dxU6XYSDv98G": "Mizuki",
+};
 
 exports.index = (req, res) => {
-  res.render('cooking_index', {type: "Public"});
+  // User validation
+  let valid_user = false;
+  let user = "Guest";
+  if ("uid" in req.query && req.query.uid in user_list) {
+    valid_user = true;
+    user = user_list[req.query.uid];
+  }
+
+  // All user may view cooking calendar, so get content
+
+  // Valid users may view their own requests, so get content
+
+  res.render('cooking_request_index', {valid_user, user});
 };
