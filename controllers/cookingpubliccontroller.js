14005d9e645d6f5868fb09d039382f49c4f3f7b7
Lennart <lentmiien@gmail.com>
2024-01-08 20:31:50 +0900

    Refactor cooking calendar rendering logic and data retrieval
    Reorganized cooking calendar rendering and data retrieval logic
    to improve code structure and readability while ensuring that only
    valid users have access to the calendar. Reworked the display of
    cooking calendar data for each day using a more concise and scalable
    approach. No issues addressed.

--- controllers/cookingpubliccontroller.js
+++ controllers/cookingpubliccontroller.js
@@ -16,17 +16,10 @@ exports.index = async (req, res) => {
     user = user_list[req.query.uid];
   }
 
-  // TODO: change to only valid users (guest users can only view cookbook)
-  // All user may view cooking calendar, so get content
-  // Get 7 dates, starting from today and get the calendar values for the dates
-  const dates_array = createDatesArray();
-  const dates_only = dates_array.map(d => d.date);
-  const calendar = await CookingCalendarModel.find({date: dates_only});
+  // Cookbook data
   const knowledge = await Chat3KnowledgeModel.find();
-
   const knowledge_lookup = [];
   knowledge.forEach(k => knowledge_lookup.push(k._id.toString()));
-
   const cooking_knowledge = knowledge.filter(d => d.data.indexOf('"name"') >= 0);// TODO: works for now, but change to something better
   cooking_knowledge.sort((a,b) => {
     if (a.title < b.title) return -1;
@@ -34,86 +27,96 @@ exports.index = async (req, res) => {
     return 0;
   });
 
-  // Generate structure holding the cooking calendar for the 7 days,
-  // fill in empty entries where there were no data from database
+  // Valid users may view cooking calendar, so get content
   const cookingCalendar = [];
-  dates_array.forEach(date => {
-    cookingCalendar.push({
-      date: date.date,
-      day: date.day,
-      lunch: {
-        name: "not set",
-        is_url: false,
-        knoledge_id: "",
-        image: "",
-      },
-      dinner: {
-        name: "not set",
-        is_url: false,
-        knoledge_id: "",
-        image: "",
-      },
-      dessert: {
-        name: "not set",
-        is_url: false,
-        knoledge_id: "",
-        image: "",
-      },
+  if (valid_user) {
+    // Get 7 dates, starting from today and get the calendar values for the dates
+    const dates_array = createDatesArray();
+    const dates_only = dates_array.map(d => d.date);
+    const calendar = await CookingCalendarModel.find({date: dates_only});
+
+    // Generate structure holding the cooking calendar for the 7 days,
+    // fill in empty entries where there were no data from database
+    dates_array.forEach(date => {
+      cookingCalendar.push({
+        date: date.date,
+        day: date.day,
+        lunch: {
+          name: "not set",
+          is_url: false,
+          knoledge_id: "",
+          image: "",
+        },
+        dinner: {
+          name: "not set",
+          is_url: false,
+          knoledge_id: "",
+          image: "",
+        },
+        dessert: {
+          name: "not set",
+          is_url: false,
+          knoledge_id: "",
+          image: "",
+        },
+      });
     });
-  });
-  calendar.forEach(c => {
-    const index = dates_only.indexOf(c.date);
-    if (index >= 0) {
-      let know_index = knowledge_lookup.indexOf(c.lunchToCook);
-      if (know_index >= 0) {
-        // Get details from knowledge entry
-        const data = JSON.parse(knowledge[know_index].data);
-        cookingCalendar[index].lunch.name = data.name;
-        cookingCalendar[index].lunch.knoledge_id = c.lunchToCook;
-        cookingCalendar[index].lunch.image = data.image;
-      } else {
-        if (c.lunchToCook.length > 0) {
-          // Assume to be URL
-          cookingCalendar[index].lunch.name = c.lunchToCook;
-          cookingCalendar[index].lunch.is_url = true;
+    calendar.forEach(c => {
+      const index = dates_only.indexOf(c.date);
+      if (index >= 0) {
+        let know_index = knowledge_lookup.indexOf(c.lunchToCook);
+        if (know_index >= 0) {
+          // Get details from knowledge entry
+          const data = JSON.parse(knowledge[know_index].data);
+          cookingCalendar[index].lunch.name = data.name;
+          cookingCalendar[index].lunch.knoledge_id = c.lunchToCook;
+          cookingCalendar[index].lunch.image = data.image;
+        } else {
+          if (c.lunchToCook.length > 0) {
+            // Assume to be URL
+            cookingCalendar[index].lunch.name = c.lunchToCook;
+            cookingCalendar[index].lunch.is_url = true;
+          }
+        }
+        know_index = knowledge_lookup.indexOf(c.dinnerToCook);
+        if (know_index >= 0) {
+          // Get details from knowledge entry
+          const data = JSON.parse(knowledge[know_index].data);
+          cookingCalendar[index].dinner.name = data.name;
+          cookingCalendar[index].dinner.knoledge_id = c.dinnerToCook;
+          cookingCalendar[index].dinner.image = data.image;
+        } else {
+          if (c.dinnerToCook.length > 0) {
+            // Assume to be URL
+            cookingCalendar[index].dinner.name = c.dinnerToCook;
+            cookingCalendar[index].dinner.is_url = true;
+          }
+        }
+        know_index = knowledge_lookup.indexOf(c.dessertToCook);
+        if (know_index >= 0) {
+          // Get details from knowledge entry
+          const data = JSON.parse(knowledge[know_index].data);
+          cookingCalendar[index].dessert.name = data.name;
+          cookingCalendar[index].dessert.knoledge_id = c.dessertToCook;
+          cookingCalendar[index].dessert.image = data.image;
+        } else {
+          if (c.dessertToCook.length > 0) {
+            // Assume to be URL
+            cookingCalendar[index].dessert.name = c.dessertToCook;
+            cookingCalendar[index].dessert.is_url = true;
+          }
         }
       }
-      know_index = knowledge_lookup.indexOf(c.dinnerToCook);
-      if (know_index >= 0) {
-        // Get details from knowledge entry
-        const data = JSON.parse(knowledge[know_index].data);
-        cookingCalendar[index].dinner.name = data.name;
-        cookingCalendar[index].dinner.knoledge_id = c.dinnerToCook;
-        cookingCalendar[index].dinner.image = data.image;
-      } else {
-        if (c.dinnerToCook.length > 0) {
-          // Assume to be URL
-          cookingCalendar[index].dinner.name = c.dinnerToCook;
-          cookingCalendar[index].dinner.is_url = true;
-        }
-      }
-      know_index = knowledge_lookup.indexOf(c.dessertToCook);
-      if (know_index >= 0) {
-        // Get details from knowledge entry
-        const data = JSON.parse(knowledge[know_index].data);
-        cookingCalendar[index].dessert.name = data.name;
-        cookingCalendar[index].dessert.knoledge_id = c.dessertToCook;
-        cookingCalendar[index].dessert.image = data.image;
-      } else {
-        if (c.dessertToCook.length > 0) {
-          // Assume to be URL
-          cookingCalendar[index].dessert.name = c.dessertToCook;
-          cookingCalendar[index].dessert.is_url = true;
-        }
-      }
-    }
-  });
+    });
+  }
 
   // Valid users may view their own requests, so get content
   let cooking_requests = [];
   if (valid_user) {
     cooking_requests = await CookingRequestModel.find({requesterName: user});
   }
+
+  // Prepare cookbook
 
   res.render('cooking_request_index', {valid_user, user, cookingCalendar, cooking_knowledge, cooking_requests});
 };
