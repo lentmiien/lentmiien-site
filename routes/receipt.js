0e5a70860bf97be94abfc406c8bcda106efc0752
Lennart <lentmiien@gmail.com>
2024-09-09 07:02:43 +0900

    Correct route types

--- routes/receipt.js
+++ routes/receipt.js
@@ -21,8 +21,8 @@ const controller = require('../controllers/receiptcontroller');
 /* GET home page. */
 router.get('/', controller.receipt);
 router.post('/upload_receipt', upload.array('imgs'), controller.upload_receipt);
-router.post('/view_receipt/:id', controller.view_receipt);
+router.get('/view_receipt/:id', controller.view_receipt);
 router.post('/correct_receipt/:id', controller.correct_receipt);
-router.post('/delete_receipt/:id', controller.delete_receipt);
+router.get('/delete_receipt/:id', controller.delete_receipt);
 
 module.exports = router;
