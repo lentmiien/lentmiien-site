204a12afa3bb3e3e45cdc08dddd7e99c6e588a83
Lennart Granstrom <lentmiien@gmail.com>
2024-09-14 23:52:40 +0900

    Improve CSV File Handling and Parsing
    Replaced the `ProcessCSV` function with an event-driven approach using the FileReader API to asynchronously read and convert CSV input to JSON. Enhanced user experience by automatically processing files if selected on page load or when newly selected. This change simplifies file handling and makes the code more robust.
    
    Removed redundant "Process" button from the UI, streamlining the interface.

--- public/js/products.js
+++ public/js/products.js
@@ -1,5 +1,53 @@
 const products = JSON.parse(document.getElementById("products").innerHTML);
 
-function ProcessCSV() {}
+const content = document.getElementById("content");
+
+// Detect input file and generate HS editor
+window.addEventListener('load', function () {
+  // These variables are used to store the input data
+  const file = {
+    dom: document.getElementById('process_file'),
+    binary: null,
+  };
+
+  // Use the FileReader API to access file content
+  const reader = new FileReader();
+
+  // Because FileReader is asynchronous, store its
+  // result when it finishes to read the file
+  reader.addEventListener('load', async function () {
+    file.binary = reader.result;
+
+    // Convert input data (csv) to JSON data
+    data = csvToJson(file.binary.toString());
+
+    console.log(data);
+  });
+
+  // At page load, if a file is already selected, read it.
+  if (file.dom.files[0]) {
+    reader.readAsText(file.dom.files[0]);
+  }
+
+  // If not, read the file once the user selects it.
+  file.dom.addEventListener('change', function () {
+    if (reader.readyState === FileReader.LOADING) {
+      reader.abort();
+    }
+    reader.readAsText(file.dom.files[0]);
+  });
+});
+
+const csvToJson = (string) => {
+  const output = [];
+  const rows = string.split(`***`);
+  rows.forEach(row => {
+    if (row.length > 0) {
+      output.push(row.split('|'));
+    }
+  });
+  return output;
+}
+
 
 function Details(product_code) {}
