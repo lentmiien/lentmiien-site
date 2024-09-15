8fe237c16a934c234140cb017cf2e3374285b64a
Lennart <lentmiien@gmail.com>
2024-09-15 11:37:13 +0900

    Add route for product deletion
    Introduced a new route to handle product deletion requests. This addition enables the server to accept and process requests for removing products from the database, thereby enhancing the product management capabilities of the application.

--- routes/product_details.js
+++ routes/product_details.js
@@ -8,5 +8,6 @@ const controller = require('../controllers/productdetailscontroller');
 router.get('/', controller.product);
 router.post('/upload_product_data', controller.upload_product_data);
 router.post('/update_product', controller.update_product);
+router.post('/delete_product', controller.delete_product);
 
 module.exports = router;
